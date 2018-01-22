ed1ea7da6f3c0190a01eb9faa3a853d6b2a5cbdd
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-22 19:54:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,7 +7,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  a = artistName;
-  delete playlist.a;
-  return playlist;
+  delete playlist.artistName;
 }
